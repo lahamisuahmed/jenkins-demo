7837bc18509eb46c0a9362f915a55f51b0b40944
Lawal A. Hamisu <lahamisuahmed@gmail.com>
2021-03-25 12:29:19 +0100

    add create pipeline for themepart

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/lahamisuahmed/themepark.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
