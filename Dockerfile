d32ed0908d8503e9b410efeb0b1deb64abb2430f
Lawal A. Hamisu <lahamisuahmed@gmail.com>
2021-03-26 13:50:29 +0100

    add all

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM jenkins/jenkins:2.271-jdk11
-
+USER root
+RUN curl -sSL https://get.docker.com/ | sh
+RUN usermod -a -G docker jenkins
+USER jenkins
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
-
 COPY seedJob.xml /usr/share/jenkins/ref/jobs/seed-job/config.xml
-
 ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
--- createJobs.groovy
+++ createJobs.groovy
@@ -21,3 +21,19 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/lahamisuahmed/themepark.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
