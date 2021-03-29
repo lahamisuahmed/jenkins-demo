b853dfe75b44bec731670b86c944a9aa2fe1fd18
Lawal A. Hamisu <lahamisuahmed@gmail.com>
2021-03-29 10:33:33 +0100

    add aws

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM jenkins/jenkins:2.271-jdk11
 USER root
 RUN curl -sSL https://get.docker.com/ | sh
-RUN usermod -a -G docker jenkins
+RUN usermod -aG docker jenkins
 USER jenkins
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
--- createJobs.groovy
+++ createJobs.groovy
@@ -37,3 +37,19 @@ pipelineJob('theme-park-job-docker') {
         }
     }
 }
+
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/lahamisuahmed/themepark.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
